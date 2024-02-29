c6287f30113041f9946e4073f5a4e2e2be2c61a2
Shymon Samsel <ssamsel@umass.edu>
2024-02-29 13:32:28 -0500

    V1

--- zybook_clicker.js
+++ zybook_clicker.js
@@ -1,11 +1,16 @@
 // Function that returns a function that clicks all elements with class className
-const clicker = className => () => Object.values(document.getElementsByClassName(className)).forEach(x => x.click());
+const clicker = (className) => () =>
+  Object.values(document.getElementsByClassName(className)).forEach((x) =>
+    x.click()
+  );
 
 // Check all 2x Speed boxes
 Object.values(document.getElementsByClassName("zb-checkbox")) // Get all checkbox divs
-  .filter(d => Object.values(d.children) // Filter them...
-    .some(t => t.innerText === "2x speed")) // ...such that the div is for a 2x speed checkbox
-  .forEach(x => x.children[0].click()); // Click/check the checkbox in the remaining divs
+  .filter((d) =>
+    Object.values(d.children) // Filter them...
+      .some((t) => t.innerText === "2x speed")
+  ) // ...such that the div is for a 2x speed checkbox
+  .forEach((x) => x.children[0].click()); // Click/check the checkbox in the remaining divs
 
 // Click all start buttons
 clicker("start-button")();
