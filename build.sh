663e1f3b28f35012bb9dd74acb0ab846b81f0516
Shymon Samsel <ssamsel@umass.edu>
2024-03-12 15:39:54 -0400

    ui

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd src
-cat DndSimulator.js short_answer.js multiple_choice.js dnd.js animations.js ../zyclicker.js
+cat DndSimulator.js short_answer.js multiple_choice.js dnd.js animations.js > ../zyclicker.js
 cd ..
